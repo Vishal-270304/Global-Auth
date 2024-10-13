7b1dea5aa7ec6bcd9e162fa9cf6112c92aeb06c2
Vishal <chawlavishal596@gmail.com>
2024-10-13 13:36:59 +0530

    Minor Fix

--- src/controllers/authControllers.ts
+++ src/controllers/authControllers.ts
@@ -147,6 +147,13 @@ export const deleteUser = async(req:Request,res:Response):Promise<void>=>{
             return;
         }
 
+        const user = await signUpModel.findById(id);
+
+        if(!user){
+            res.status(404).json({error:"User not found"});
+            return;
+        }
+
         const deletedUser = await signUpModel.findByIdAndDelete(id);
 
         res.status(200).json({message:"User deleted successfully"});
